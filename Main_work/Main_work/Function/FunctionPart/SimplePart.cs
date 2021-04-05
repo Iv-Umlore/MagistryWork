18c3774bb899098a5551deacb20ae8823dbea232
Iv-Umlore <iv.kiselev1998@yandex.ru>
2021-04-05 19:36:54 +0300

    Add interface for function

---
+++ Main_work/Main_work/Function/FunctionPart/SimplePart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_work.Function.FunctionPart
+{
+    /// <summary>
+    /// Класс представляющий собой один из множителей Term
+    /// </summary>
+    public class SimplePart
+    {
+    }
+}
--- Main_work/Main_work/Function/FunctionPart/Term.cs
+++ Main_work/Main_work/Function/FunctionPart/Term.cs
@@ -9,8 +9,29 @@ namespace Main_work.Function.FunctionPart
     public class Term
     {
         private List<string> _variables;
+        private List<SimplePart> _parts;
 
         public Term(string functionPart)
+        {
+
+        }
+
+        public List<string> GetVariables()
+        {
+            return _variables;
+        }
+        
+        public void FixValue(string variable, double value)
+        {
+
+        }
+
+        public double GetValue()
+        {
+            return 0.0;
+        }
+
+        public void CanselFix(string variable)
         {
 
         }
